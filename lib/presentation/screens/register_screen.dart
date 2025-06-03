147b297553d6dd508bbbc0b29b0a970885e74389
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-06-02 19:21:31 -0500

    feat: Add custom text form field widget

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_forms/presentation/widgets/widgets.dart';
 
 class RegisterScreen extends StatelessWidget {
   static const name = 'register-screen';
@@ -19,6 +20,55 @@ class _RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const FlutterLogo(size: 100),
+              const _RegisterForm(),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RegisterForm extends StatelessWidget {
+  const _RegisterForm();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          CustomTextFormField(
+            label: 'Username',
+            iconSuffix: Icons.supervised_user_circle_outlined,
+          ),
+          const SizedBox(height: 10),
+          CustomTextFormField(
+            label: 'e-mail',
+            iconSuffix: Icons.email_outlined,
+          ),
+          const SizedBox(height: 10),
+          CustomTextFormField(
+            label: 'Password',
+            obscureText: true,
+            iconSuffix: Icons.password,
+          ),
+          const SizedBox(height: 20),
+          FilledButton.tonalIcon(
+            onPressed: () {},
+            icon: const Icon(Icons.save),
+            label: const Text('Create User'),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/presentation/widgets/shared/inputs/custom_text_form_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final bool obscureText;
+  final IconData? iconSuffix;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+
+  const CustomTextFormField({
+    super.key,
+    this.label,
+    this.hint,
+    this.obscureText = false,
+    this.iconSuffix,
+    this.errorMessage,
+    this.onChanged,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final border = OutlineInputBorder(borderRadius: BorderRadius.circular(10));
+
+    return TextFormField(
+      onChanged: onChanged,
+      validator: validator,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        enabledBorder: border,
+        focusedBorder: border.copyWith(
+          borderSide: BorderSide(color: colors.primary),
+        ),
+        errorBorder: border.copyWith(
+          borderSide: BorderSide(color: colors.error),
+        ),
+        focusedErrorBorder: border.copyWith(
+          borderSide: BorderSide(color: colors.primary),
+        ),
+        isDense: true,
+        label: Text(label ?? ''),
+        hintText: hint,
+        errorText: errorMessage,
+        focusColor: colors.primary,
+        suffix: Icon(iconSuffix, color: colors.primary),
+      ),
+    );
+  }
+}
