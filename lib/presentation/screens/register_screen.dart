1ee3759db0768cffcc7865c16f0cb80d271d923c
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-06-02 19:37:49 -0500

    feat: Add traditional form validations

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -38,32 +38,74 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
           CustomTextFormField(
             label: 'Username',
             iconSuffix: Icons.supervised_user_circle_outlined,
+            onChanged: (value) => username = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Field required';
+              if (value.trim().isEmpty) return 'Field required';
+              if (value.length < 6) {
+                return 'Username must be longer than six characters';
+              }
+              return null;
+            },
           ),
           const SizedBox(height: 10),
           CustomTextFormField(
             label: 'e-mail',
             iconSuffix: Icons.email_outlined,
+            onChanged: (value) => email = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Field required';
+              if (value.trim().isEmpty) return 'Field required';
+              final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+              if (!emailRegExp.hasMatch(value)) {
+                return 'Field does not have email format';
+              }
+              return null;
+            },
           ),
           const SizedBox(height: 10),
           CustomTextFormField(
             label: 'Password',
             obscureText: true,
             iconSuffix: Icons.password,
+            onChanged: (value) => password = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Field required';
+              if (value.trim().isEmpty) return 'Field required';
+              if (value.length < 6) {
+                return 'Password must be longer than six characters';
+              }
+              return null;
+            },
           ),
           const SizedBox(height: 20),
           FilledButton.tonalIcon(
-            onPressed: () {},
+            onPressed: () {
+              final isValid = _formKey.currentState!.validate();
+              if (!isValid) return;
+            },
             icon: const Icon(Icons.save),
             label: const Text('Create User'),
           ),
